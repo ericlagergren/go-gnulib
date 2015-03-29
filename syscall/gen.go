60d71fe2d85d743e81e19cb0834794857352b85d
Eric <ericscottlagergren@gmail.com>
2015-03-28 22:56:10 -0700

    added ioctl

---
+++ syscall/gen.go
@@ -0,0 +1,21 @@
+package _syscall
+
+var (
+	errEAGAIN error = syscall.EAGAIN
+	errEINVAL error = syscall.EINVAL
+	errENOENT error = syscall.ENOENT
+)
+
+func ErrnoErr(e syscall.Errno) error {
+	switch e {
+	case 0:
+		return nil
+	case syscall.EAGAIN:
+		return errEAGAIN
+	case syscall.EINVAL:
+		return errEINVAL
+	case syscall.ENOENT:
+		return errENOENT
+	}
+	return e
+}
---
+++ syscall/ioctl.go
@@ -0,0 +1,19 @@
+package _syscall
+
+import (
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+// File descriptor, a request, and a memory address
+func Ioctl(fd int, request int, argp *int) (err error) {
+	_, _, e1 := syscall.Syscall(syscall.SYS_IOCTL,
+		uintptr(fd),
+		uintptr(request),
+		uintptr(unsafe.Pointer(argp)))
+	if e1 != 0 {
+		err = ErrnoErr(e1)
+	}
+	return
+}
